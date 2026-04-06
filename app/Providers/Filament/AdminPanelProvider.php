1436ff006a6d2e4d3011bdd7a18877c3ff1f62ae
Anibal Alvarez <aalvarez@chmw.co>
2026-04-06 18:32:29 -0400

    refactor: consolidate session security into VerifyReCaptcha pipeline as primary defense

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -113,7 +113,6 @@ class AdminPanelProvider extends PanelProvider
             )
             ->authMiddleware([
                 Authenticate::class,
-                \App\Http\Middleware\CheckLogoutAt::class,
             ]);
     }
 }
