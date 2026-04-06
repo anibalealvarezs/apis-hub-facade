c3bfd34fbca401a0b42211a26a55fdceed657eae
Anibal Alvarez <aalvarez@chmw.co>
2026-04-06 19:00:45 -0400

    fix: final commit for Octane cache clear testing without any garbage data

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            \App\Http\Middleware\CheckLogoutAt::class,
-        ]);
         $middleware->trustProxies(at: '*');
         $middleware->validateCsrfTokens(except: [
             'api/heartbeat',
