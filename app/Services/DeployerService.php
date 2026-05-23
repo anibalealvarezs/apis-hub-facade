c8b909a50a744c4b8bbdf0ebb6ed77cdf3802abb
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-23 17:37:45 -0400

    Fix for docker command

--- app/Services/DeployerService.php
+++ app/Services/DeployerService.php
@@ -62,7 +62,7 @@ class DeployerService
 
         // 3. Fire deployment (full-deploy.sh)
         // Clean up any existing containers or orphans from failed previous deployments to avoid naming conflicts
-        $commands[] = "CONTAINERS=\\$(docker ps -aq --filter name=apis-hub-{$project->subdomain}-) && if [ ! -z \"\\$CONTAINERS\" ]; then docker rm -f \\$CONTAINERS; fi";
+        $commands[] = "docker ps -aq --filter name=apis-hub-{$project->subdomain}- | xargs -r docker rm -f || true";
         $commands[] = "cd {$path} && sh bin/full-deploy.sh";
 
         // 4. Register in Caddy (Reverse Proxy for the specific container)
