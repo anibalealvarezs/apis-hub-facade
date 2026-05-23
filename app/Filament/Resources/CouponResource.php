808348ffdfc8a16a37e13fb9f23f5dbf0ff9daa2
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-22 22:17:43 -0400

    Fixed CouponResource

--- app/Filament/Resources/CouponResource.php
+++ app/Filament/Resources/CouponResource.php
@@ -77,10 +77,8 @@ class CouponResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Columns\Layout\Split::make([
-                    Tables\Actions\EditAction::make(),
-                    Tables\Actions\DeleteAction::make(),
-                ]),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
