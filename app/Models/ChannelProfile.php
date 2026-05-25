7d620520e8bd7c5410f2cfa9fee4a626951e78c2
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-24 23:05:43 -0400

    Channel Profiles model implemented

---
+++ app/Models/ChannelProfile.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ChannelProfile extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'provider',
+        'provider_account_id',
+        'name',
+        'email',
+        'access_token',
+        'refresh_token',
+        'expires_at',
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    /**
+     * Get the user that owns the profile.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
