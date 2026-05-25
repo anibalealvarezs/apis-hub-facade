89110afd0e21d85b8281ab92ae54319acebbbe6b
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-25 14:16:24 -0400

    Top bar logo fixed

--- resources/views/filament/hooks/topbar-logo.blade.php
+++ resources/views/filament/hooks/topbar-logo.blade.php
@@ -1,12 +1,14 @@
 <a 
     href="{{ filament()->getHomeUrl() ?? url('/') }}" 
     class="flex items-center me-6" 
-    x-show="!$store.sidebar.isOpen"
+    x-show="!$store.sidebar.isOpen" 
+    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0 translate-x-[-10px]"
+    x-transition:enter-end="opacity-100 translate-x-0"
 >
-    <span class="font-bold text-red-500">TESTING TOPBAR</span>
     <!-- Light Mode Logo -->
     <img src="{{ asset('images/branding/apishub-trans-620.webp') }}" class="h-10 w-auto dark:hidden" alt="APIs Hub" />
     
     <!-- Dark Mode Logo -->
-    <img src="{{ asset('images/branding/apishub-trans-light-620.webp') }}" class="h-10 w-auto hidden dark:block" alt="APIs Hub" />
+    <img src="{{ asset('images/branding/apishub-trans-light-620.webp') }}" class="h-10 w-auto hidden dark:flex" alt="APIs Hub" />
 </a>
