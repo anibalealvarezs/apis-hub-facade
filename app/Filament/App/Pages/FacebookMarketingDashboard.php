516c7ece3bdd1dbd9bf79c854913a052227add31
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-06-01 14:37:58 -0400

    Fix for FB marketing dashboard

--- app/Filament/App/Pages/FacebookMarketingDashboard.php
+++ app/Filament/App/Pages/FacebookMarketingDashboard.php
@@ -47,7 +47,7 @@ class FacebookMarketingDashboard extends Page
             $service = app(\App\Services\RemoteEngineService::class);
             $tenant = Filament::getTenant();
             
-            $response = $service->listChanneled($tenant, 'facebook_marketing', 'meta_ad_account');
+            $response = $service->listChanneled($tenant, 'facebook_marketing', 'channeled_account');
 
             if (isset($response['data']) && is_array($response['data'])) {
                 foreach ($response['data'] as $account) {
