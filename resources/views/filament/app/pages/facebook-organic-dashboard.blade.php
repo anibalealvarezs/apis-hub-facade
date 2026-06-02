c02bd8929fec3e4ab5517dce0fad534c51896e4a
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-06-01 22:10:11 -0400

    Dashboards UI styled

--- resources/views/filament/app/pages/facebook-organic-dashboard.blade.php
+++ resources/views/filament/app/pages/facebook-organic-dashboard.blade.php
@@ -1,12 +1,4 @@
 <x-filament-panels::page>
-    <div class="flex items-center justify-between mb-6">
-        <div>
-            <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white flex items-center gap-3">
-                <x-heroicon-o-users class="w-8 h-8 text-[#1877F2]" />
-                {{ __('Meta Pages & Instagram Accounts') }}
-            </h1>
-        </div>
-    </div>
 
     <div class="p-8 bg-white dark:bg-white/5 border border-gray-200 dark:border-white/10 rounded-xl shadow-sm text-center">
         <x-heroicon-o-wrench-screwdriver class="w-16 h-16 mx-auto mb-4 text-gray-400 opacity-50" />
