263305b02682e3767bae8c566cfebcde7568ddb0
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-06-11 19:33:10 -0400

    Fixed Admin panel's grid

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -98,10 +98,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Pages\Dashboard::class,
-            ])
-            ->widgetGridColumns([
-                'default' => 12,
+                \App\Filament\Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
