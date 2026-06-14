d2498db68ad362964119ffce137f24041996a143
Anibal Alvarez <aalvarez@chmw.co>
2026-06-13 22:29:47 -0400

    fix: revert to working return statement with double quotes - HTML entity &quot; is safe but &#039; breaks JS

--- resources/views/filament/app/pages/data-sources.blade.php
+++ resources/views/filament/app/pages/data-sources.blade.php
@@ -63,7 +63,7 @@
                 }
 
                 if (!color) return '';
-                return `<span class='inline-block w-2.5 h-2.5 rounded-full ${color} flex-shrink-0' title='${label.replace(/'/g, '&#039;')}'></span>`;
+                return `<span class="inline-block w-2.5 h-2.5 rounded-full ${color} flex-shrink-0" title="${label.replace(/"/g, '&quot;')}"></span>`;
             },
 
             get formAssets() {
