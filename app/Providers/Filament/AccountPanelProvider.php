4badc500104cff945450409b9cd178115cbaa581
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-06-30 17:36:12 -0400

    Billing Accounts requests recovered

--- app/Providers/Filament/AccountPanelProvider.php
+++ app/Providers/Filament/AccountPanelProvider.php
@@ -51,7 +51,7 @@ class AccountPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Account/Widgets'), for: 'App\\Filament\\Account\\Widgets')
             ->widgets([
-                // \App\Filament\Account\Widgets\BillingRequestsWidget::class,
+                \App\Filament\Account\Widgets\BillingRequestsWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
