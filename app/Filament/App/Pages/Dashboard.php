eef2788d822b20058400c9d2da4b41d6e43071f9
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-07-07 12:05:08 -0400

    Remove Heading and crud buttons from the main dashboard page

--- app/Filament/App/Pages/Dashboard.php
+++ app/Filament/App/Pages/Dashboard.php
@@ -11,11 +11,6 @@ class Dashboard extends BaseDashboard
 
     protected static string $view = 'filament.app.pages.home-dashboard';
 
-    public function getTitle(): string | \Illuminate\Contracts\Support\Htmlable
-    {
-        return isset($this->dashboard) ? $this->dashboard->name : __('Dashboard');
-    }
-
     public function getWidgets(): array
     {
         return [];
@@ -36,24 +31,5 @@ class Dashboard extends BaseDashboard
         if ($default) {
             $this->loadDashboardViewData($default);
         }
-    }
-
-    protected function getHeaderActions(): array
-    {
-        if (!isset($this->dashboard)) {
-            return [];
-        }
-
-        return [
-            \Filament\Actions\Action::make('edit')
-                ->label(__('Edit Dashboard'))
-                ->icon('heroicon-o-pencil-square')
-                ->url(DashboardResource::getUrl('builder', ['record' => $this->dashboard]))
-                ->visible(fn () => auth()->user()->can('edit_preferences')),
-            \Filament\Actions\Action::make('all_dashboards')
-                ->label(__('All Dashboards'))
-                ->icon('heroicon-o-squares-2x2')
-                ->url(DashboardResource::getUrl('index')),
-        ];
     }
 }
