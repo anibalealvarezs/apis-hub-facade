a9b72d93473949eb69204efde003c7087d25f612
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-07-06 23:12:56 -0400

    inbox box moved to the heading the modal

--- resources/views/filament/app/pages/dashboard-builder.blade.php
+++ resources/views/filament/app/pages/dashboard-builder.blade.php
@@ -282,7 +282,7 @@
                     <div class="flex-1 bg-gray-50 dark:bg-gray-900 min-h-0 overflow-y-auto desktop-overflow-hidden relative">
                         <div class="modal-body-absolute-wrapper-custom flex flex-col md:flex-row gap-6">
                             {{-- Left Column: Global Configuration --}}
-                            <div class="flex flex-col gap-6 overflow-y-auto custom-scrollbar pr-2 pb-2 min-h-0" style="flex: 1 1 250px; max-width: 100%; height: 100%;">
+                            <div class="flex flex-col gap-6 overflow-y-auto custom-scrollbar pr-2 pb-2 min-h-0" style="flex: 1 1 250px; max-width: 100%; height: 100%; padding-right: 5px;">
 
                         {{-- Card: Identity --}}
                         <div class="rounded-xl border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 shadow-sm overflow-hidden flex-shrink-0">
