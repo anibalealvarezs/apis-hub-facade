3c6ed7851ed7ebdc06352e62f76f84dfda96a657
Anibal Alvarez <aalvarez@chmw.co>
2026-08-08 20:17:50 -0400

    fix(dashboard-builder): add explicit height constraint to left config column for vertical scrolling

--- resources/views/filament/app/pages/dashboard-builder.blade.php
+++ resources/views/filament/app/pages/dashboard-builder.blade.php
@@ -439,7 +439,7 @@
                     <div class="modal-body-absolute-wrapper flex flex-col md:flex-row gap-6 flex-1 min-h-0">
                         {{-- Left Column: Global Configuration --}}
                         <div
-                            class="flex flex-col gap-6 overflow-y-auto custom-scrollbar pr-2 pb-2 min-h-0 bd-config-col"
+                            class="flex flex-col gap-6 overflow-y-auto custom-scrollbar pr-2 pb-2 h-full max-h-full min-h-0 bd-config-col"
                             :class="{ 'hidden md:flex': activeMobileTab !== 'config' }">
 
                             {{-- Card: Identity --}}
