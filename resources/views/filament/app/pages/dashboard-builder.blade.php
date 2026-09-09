ce3b1aa5a6ba58a8f1f6ab0bed995edd181c0040
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-09-08 22:13:05 -0400

    Refine widget's series config

--- resources/views/filament/app/pages/dashboard-builder.blade.php
+++ resources/views/filament/app/pages/dashboard-builder.blade.php
@@ -1059,39 +1059,43 @@
                                                     {{-- Channel & Metric (Editable for Raw Metric Series) --}}
                                                     <template x-if="series.type !== 'derived_metric'">
                                                         <div class="flex flex-col gap-4">
-                                                            <div>
-                                                                <label
-                                                                    class="block text-xs font-semibold text-gray-700 dark:text-gray-300 mb-2">{{ __('Channel') }}</label>
-                                                                <x-ui.select-input x-model="series.channel"
-                                                                                   x-on:change="onWidgetRawChannelChange(index)"
-                                                                                   x-init="$nextTick(() => { $el.value = series.channel })"
-                                                                                   class="w-full">
-                                                                    <x-ui.select-option
-                                                                        value="">{{ __('Select a channel...') }}</x-ui.select-option>
-                                                                    <template x-for="(label, key) in channels" :key="key">
-                                                                        <x-ui.select-option x-bind:value="key"
-                                                                                            x-text="label"></x-ui.select-option>
-                                                                    </template>
-                                                                </x-ui.select-input>
-                                                            </div>
-
-                                                            {{-- Data Scope / Matrix (Per-Series) --}}
-                                                            <template x-if="series.channel && allChannelDependencies[series.channel] && Object.keys(allChannelDependencies[series.channel]).length > 0">
+                                                            {{-- Channel & Scope Row (Side-by-side) --}}
+                                                            <div class="grid grid-cols-1 gap-4"
+                                                                 :class="(series.channel && allChannelDependencies[series.channel] && Object.keys(allChannelDependencies[series.channel]).length > 0) ? 'sm:grid-cols-2' : ''">
                                                                 <div>
                                                                     <label
-                                                                        class="block text-xs font-semibold text-gray-700 dark:text-gray-300 mb-2">{{ __('Data Scope / Matrix') }}</label>
-                                                                    <x-ui.select-input x-model="series.dependency"
-                                                                                       x-on:change="onWidgetRawSeriesDependencyChange(index)"
-                                                                                       x-init="$nextTick(() => { if (series.dependency) $el.value = series.dependency })"
+                                                                        class="block text-xs font-semibold text-gray-700 dark:text-gray-300 mb-2">{{ __('Channel') }}</label>
+                                                                    <x-ui.select-input x-model="series.channel"
+                                                                                       x-on:change="onWidgetRawChannelChange(index)"
+                                                                                       x-init="$nextTick(() => { $el.value = series.channel })"
                                                                                        class="w-full">
-                                                                        <template x-for="(label, key) in allChannelDependencies[series.channel]" :key="key">
+                                                                        <x-ui.select-option
+                                                                            value="">{{ __('Select a channel...') }}</x-ui.select-option>
+                                                                        <template x-for="(label, key) in channels" :key="key">
                                                                             <x-ui.select-option x-bind:value="key"
-                                                                                                x-bind:selected="series.dependency === key"
                                                                                                 x-text="label"></x-ui.select-option>
                                                                         </template>
                                                                     </x-ui.select-input>
                                                                 </div>
-                                                            </template>
+
+                                                                {{-- Data Scope / Matrix (Per-Series) --}}
+                                                                <template x-if="series.channel && allChannelDependencies[series.channel] && Object.keys(allChannelDependencies[series.channel]).length > 0">
+                                                                    <div>
+                                                                        <label
+                                                                            class="block text-xs font-semibold text-gray-700 dark:text-gray-300 mb-2">{{ __('Data Scope / Matrix') }}</label>
+                                                                        <x-ui.select-input x-model="series.dependency"
+                                                                                           x-on:change="onWidgetRawSeriesDependencyChange(index)"
+                                                                                           x-init="$nextTick(() => { if (series.dependency) $el.value = series.dependency })"
+                                                                                           class="w-full">
+                                                                            <template x-for="(label, key) in allChannelDependencies[series.channel]" :key="key">
+                                                                                <x-ui.select-option x-bind:value="key"
+                                                                                                    x-bind:selected="series.dependency === key"
+                                                                                                    x-text="label"></x-ui.select-option>
+                                                                            </template>
+                                                                        </x-ui.select-input>
+                                                                    </div>
+                                                                </template>
+                                                            </div>
 
                                                             <div class="my-1 flex flex-col shrink-0">
                                                                 <div class="flex items-center justify-between mb-2">
